9eb6ba1680802642ce2f16b8ac9f99a2df694b10
Pedro Reis <phbarcelosreis66@gmail.com>
2022-10-24 00:58:10 -0300

    error cambo body invalido

--- src/pages/Cadastro/Cadastro.js
+++ src/pages/Cadastro/Cadastro.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Logo from "../../assets/images/Group8.png"
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 
 const LogIn = styled.form`
@@ -47,6 +51,7 @@ const Button = styled.button`
     align-items: center;
     justify-content: center;
     cursor: pointer;
+    margin-bottom: 25px;
     & p {
     font-family: 'Lexend Deca';
     font-style: normal;
@@ -59,16 +64,54 @@ const Button = styled.button`
 
 function Cadastro() {
 
+    /* States Locais */
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const [nome, setNome] = useState("");
+    const [foto, setFoto] = useState("");
+    const [desabilitar, setDesabilitar] = useState(false);
+
+    /* States do Autenticador / Navigate / Context */
+    const Navegar = useNavigate();
+    const Api = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up";
+
+    function Register(){
+
+        const emailUser = {
+            email: email,
+            senha: senha,
+            nome: nome,
+            foto: foto
+        }
+        
+        const promessa = axios.post(Api, emailUser);
+        setDesabilitar(true);
+        
+        promessa.then(() => {
+            Navegar("/")
+        });
+
+        promessa.catch((e) => {
+            setDesabilitar(false);
+            alert(e.response.data.message);
+        });
+
+    }
+
+    function Criado(e){
+        e.preventDefault();
+        Register();
+    }
 
     return (
-        <LogIn>
+        <LogIn onSubmit={Criado}>
             <img src={Logo} alt="Logo Cadastro" />
-            <input placeholder="email" />
-            <input placeholder="senha" />
-            <input placeholder="nome" />
-            <input placeholder="foto" />
+            <input onChange={e => setEmail(e.target.value)} disabled={desabilitar} type="email" placeholder="email" required/>
+            <input onChange={e => setSenha(e.target.value)} disabled={desabilitar} type="password" placeholder="senha" required/>
+            <input onChange={e => setNome(e.target.value)} disabled={desabilitar} placeholder="nome" required/>
+            <input onChange={e => setFoto(e.target.value)} disabled={desabilitar} type="url" placeholder="foto" required/>
             <Button>Cadastrar</Button>
-            <p>Já tem uma conta? Faça login!</p>
+            <Link to="/" ><p>Já tem uma conta? Faça login!</p></Link>
         </LogIn>
 
     )
